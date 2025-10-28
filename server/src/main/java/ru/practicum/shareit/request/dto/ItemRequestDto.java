bb4b57773a1d894e3764c837373810cc6c5ede9f
MGR30 <sir.mgr24@yandex.ru>
2025-10-28 21:55:40 +0300

    Разделяет проект на отдельные модули

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.item.dto.ItemDto;
@@ -8,9 +7,7 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-/**
- * TODO Sprint add-item-requests.
- */
+
 @Data
 @Builder
 public class ItemRequestDto {
